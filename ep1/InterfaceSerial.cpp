ff597b0ce85022f541a8def5d7427a83d97f49f9
andrebarbs13 <31722863+andrebarbs13@users.noreply.github.com>
2017-10-01 17:18:59 -0300

    Update InterfaceSerial.cpp

--- ep1/InterfaceSerial.cpp
+++ ep1/InterfaceSerial.cpp
@@ -7,7 +7,7 @@ void InterfaceSerial::inicializar(string porta) {
  nomes[4] = "T5";
 }
 double InterfaceSerial::getValor(string canal) {
- double valoresT1[] = {10, -27, 30, 40, 23, 0.012, 345, 23, -12, -0.002, 12 ,3};
+ double valoresT1[] = {10, -27, 30, 40, 13, 0.012, 345, 23, -12, -0.002, 12 ,3};
  double valoresT2[] = {10, 0.00001, 0.6, 25, 23, -0.002,5,1,2};
  if (canal == "T1") return valoresT1[numeroDeChamadas];
  return valoresT2[numeroDeChamadas];
